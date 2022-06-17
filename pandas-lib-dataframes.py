dec0a60ab1062a19dee5b72038bd190d5264dc51
Justin Griffin <103063031+justingriff3@users.noreply.github.com>
2022-06-17 15:51:38 -0400

    Update pandas-lib-dataframes.py

--- pandas-lib-dataframes.py
+++ pandas-lib-dataframes.py
@@ -34,6 +34,8 @@ df = pd.DataFrame(students, index = ['school1', 'school2','school3'])
 
 df.head()
 
+
+
 df.loc["school2"]  #Returns a tubular display with data that is indexed as 'school1'
 df.loc['school1', 'Name'] #Returns only the names of records with a index of 'school1'
 
@@ -58,3 +60,63 @@ del copy_df['Class']
 
 df['ClassRanking'] = None #Initializing a new column with no values
 
+#DATA FRAME: INDEXING
+df = pd.DataFrame([{'COA': 0.92,
+       'GRE Score': 337,
+       'SOP' : 4.5, 
+        'Research' : 1,
+        "Serial Number" : 1,
+        "CGPA": 9.65,
+         "LOR": 4.5,
+         "University Rating": 4,
+           "TOEFL": 118},
+        {'COA': 0.76,
+       'GRE Score': 324,
+       'SOP' : 4.0, 
+        'Research' : 1,
+        "Serial Number" : 2,
+        "CGPA": 8.87,
+         "LOR": 4.5,
+         "University Rating": 4,
+           "TOEFL": 107},
+           {'COA': 0.72,
+       'GRE Score': 316,
+       'SOP' : 3.0, 
+        'Research' : 1,
+        "Serial Number" : 3,
+        "CGPA": 8.00,
+         "LOR": 3.5,
+         "University Rating": 3,
+           "TOEFL": 104},
+           {'COA': 0.80,
+       'GRE Score': 322,
+       'SOP' : 3.5, 
+        'Research' : 0,
+        "Serial Number" : 4,
+        "CGPA": 8.67,
+         "LOR": 2.5,
+         "University Rating": 3,
+           "TOEFL": 110},
+           {'COA': 0.65,
+       'GRE Score': 314,
+       'SOP' : 2.0, 
+        'Research' : 0,
+        "Serial Number" : 5,
+        "CGPA": 8.21,
+         "LOR": 3.0,
+         "University Rating": 2,
+           "TOEFL": 103}])
+
+new_df = df.rename(columns = {'LOR' : 'Letter of Recommendation', 
+                             "SOP": "Statement of Purpose"}) #Function to rename a column
+new_df.columns #attribute to view columns
+
+# pass strip() function into mapper parameter strip white spaces. Very useful for accurate data cleaning
+new_df = new_df.rename(mapper = str.strip, axis = "columns")
+
+#Using the .columns attribute you can assign a list of column names which will
+#-directly rename the columns. An example can be make all the column names uppercase
+cols = list(df.columns)
+cols = [x.upper().strip() for x in cols]
+df.columns = cols
+df
