dd908d05203fc51ad0a087110df561ac2a681000
Justin Griffin <103063031+justingriff3@users.noreply.github.com>
2022-06-17 09:44:54 -0400

    Update pandas-lib-DS.py

--- pandas-lib-DS.py
+++ pandas-lib-DS.py
@@ -1,5 +1,39 @@
 import pandas as pd
+import numpy as np
 
+numbers = [1,2,3]
+
+pd.Series[numbers]
+
+
+#NAN VALUES (NUMPY)
+np.nan = None
+np.nan == np.nan  #Returns false be cause NaN does equate to none
+
+np.isnan(np.nan)  #isnan is a special function to test for the presence of not a number, useful for querying a Series/Dataframe
+
+#Creating a series from dictionary data. Note: The index will auto assign to the keys of the dict
+student_scores = {'Justin' : 'Astrophysics',
+                   'Siroun' : 'Microbiology',
+                   'Addison' : 'Neuroscience'}
+
+s = pd.Series(student_scores)
+s 
+
+s.index #Displays all indexes in the form of a list
+
+students = [("Justin", 'Griffin'),('Siroun', 'Elliot'), ('Addison', 'Ruffin')] 
+ss = pd.Series(students)   #Create a series from a list of tuples. Note: Index is still int from 0-N
+
+ss = pd.Series(students, index = [333, 454, 782])
+
+
+
+
+
+
+
+#DATA FRAMES 
 students = ["Justin", "Siroun", "Addison"]
 pd.Series(students)
 
@@ -23,3 +57,4 @@ df.head()
 df.loc["school1"]
 
 df.T.loc["Name"]
+
