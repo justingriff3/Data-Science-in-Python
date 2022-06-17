ba3a55322a47b136526723b68bf38ce155afe1ff
Justin Griffin <103063031+justingriff3@users.noreply.github.com>
2022-06-17 10:56:43 -0400

    Update pandas-lib-dataframes.py

--- pandas-lib-dataframes.py
+++ pandas-lib-dataframes.py
@@ -1,1 +1,25 @@
+#DATA FRAMES 
+students = ["Justin", "Siroun", "Addison"]
+pd.Series(students)
+
+record1 = pd.Series({'Name': "Justin",
+                    'Class': 'Data Engineering',
+                    'Score': 89})
+
+record2 = pd.Series({'Name': "Siroun",
+                    'Class': 'Artifical Inteligence',
+                    'Score': 92})
+
+record3 = pd.Series({'Name': "Addison",
+                    'Class': 'Automation',
+                    'Score': 85})
+
+df = pd.DataFrame([record1, record2, record3],
+                 index = ['school1', 'school2', 'school1'])
+
+df.head()
+
+df.loc["school1"]
+
+df.T.loc["Name"]
 
