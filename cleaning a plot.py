7ded924b6505a16467cd167b8acc313957f9c49f
Justin Griffin <103063031+justingriff3@users.noreply.github.com>
2022-06-21 12:33:14 -0400

    Update cleaning a plot.py

--- cleaning a plot.py
+++ cleaning a plot.py
@@ -17,6 +17,9 @@ plt.xticks(pos, game_platforms)
 plt.ylabel('% Popularity')
 plt.title('Top 5 Gaming platforms', alpha = 0.8)
 
+#remove all the ticks (both axes) and tick labels on the Y axis
+plt.tick_params(top = 'off', bottom = 'off', left = 'off', right = 'off', labelleft = 'off', labelbottom = 'on')
+
 for spine in plt.gca().spines.values():  #Remove the frame of the chart
     spine.set_visible(False)
 
