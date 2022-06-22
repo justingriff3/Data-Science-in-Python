4c65c3f19786eff73b27a3c4ced3b71b24315f09
Justin Griffin <103063031+justingriff3@users.noreply.github.com>
2022-06-22 10:46:36 -0400

    Update matplotlib-lib-Basics.py

--- matplotlib-lib-Basics.py
+++ matplotlib-lib-Basics.py
@@ -49,4 +49,36 @@ xvals = range(len(linear_data))
 plt.barh(xvals, linear_data, height = 0.3, color = 'blue')
 plt.barh(xvals, quadratic_data, height = 0.3, left = linear_data, color = 'r')
 
+#SUBPLOTS
+plt.figure()
+plt.subplot(1, 2, 1) # 1 row with 2 column, sets first axis to be current axis
 
+plt.plot(linear_data, '-o')
+
+exponential_data = linear_data**2
+
+#Initializes subplots
+plt.subplot(1, 2, 2)
+plt.plot(exponential_data, '-o')
+
+plt.subplot(1, 2, 1)
+plt.plot(exponential_data, '-x')
+
+#Creates second plot with exponential graph
+plt.figure() 
+axl = plt.subplot(1, 2, 1)
+plt.plot(linear_data, '-o')
+ax2 = plt.subplot(1, 2, 2, sharey=axl)
+plt.plot(exponential_data, '-x')
+
+plt.figure()
+plt.subplot(1, 2, 1) == plt.subplot(121) #Returns True
+
+fig, ((ax1, ax2, ax3), (ax4, ax5, ax6), (ax7, ax8, ax9)) = plt.subplots(3, 3, sharex = True, sharey = True)
+ax5.plot(linear_data, '-')  #Three by three grid wth the x and y axis locked
+
+for ax in plt.gcf().get_axes():   #gcf : get current figure
+    for label in ax.get_xticklabels() + ax.get_yticklabels():  #Makes the x and y tick labels true
+        label.set_visible(True)
+        
+plt.gcf().canvas.draw()
